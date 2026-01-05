ac7d463f0c07b75b3da55e75039d7422c4c2d4d8
sshamimohd-debug <sshamimohd@gmail.com>
2026-01-05 17:18:12 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = "mpgbpl-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./data.js",
+  "./storage.js",
+  "./app.js",
+  "./manifest.json",
+];
+
+self.addEventListener("install",(e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate",(e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))
+  );
+});
+
+self.addEventListener("fetch",(e)=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res || fetch(e.request))
+  );
+});
