3fe7f6cfcae486f1da12e4b0a64a060126f1cfee
sshamimohd-debug <sshamimohd@gmail.com>
2026-01-06 11:06:57 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mpgbpl-cache-v1";
+const CACHE = "mpgbpl-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,20 +7,39 @@ const ASSETS = [
   "./storage.js",
   "./app.js",
   "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
 ];
 
-self.addEventListener("install",(e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate",(e)=>{
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch",(e)=>{
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res=>res || fetch(e.request))
+    caches.match(e.request).then((res) =>
+      res ||
+      fetch(e.request)
+        .then((net) => {
+          // cache runtime GET requests (best effort)
+          if (e.request.method === "GET" && net && net.status === 200) {
+            const copy = net.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, copy));
+          }
+          return net;
+        })
+        .catch(() => res)
+    )
   );
 });
